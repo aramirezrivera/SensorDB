8f8123bd865475083489c08d67ff95b677d5ba63
Alejandro <alejandro.ramirez2@upr.edu>
2023-07-10 20:38:42 -0400

    added emailGraph method on graphGenerator class

--- SensorOrchestrator.py
+++ SensorOrchestrator.py
@@ -2,21 +2,23 @@ import sensorDHT22
 import time
 import threading
 import graphGenerator
+import config
 
 def sensor1_thread():
     sensorInside1 = sensorDHT22.sensorDHT22(4)
 
     while True:
         # Store Inside sensor1 data in the sql database
-        sensorInside1.write()
+        #sensorInside1.write()
         # Sleep for 30 minutes
         time.sleep(1800)
 def graph_thread():
-    grapher = graphGenerator.graphGenerator(4)
+    grapher = graphGenerator.graphGenerator(config.destination_email)
     while True:
         #print("This is the
         grapher.createGraph()
-        time.sleep(20)
+        grapher.emailGraph()
+        time.sleep(30)
         #time.sleep(86400)
      
 if __name__ =="__main__":
